088f48d9edabfe715c6ef8e03fa87a146e07ff1e
Arunjith <arunjithk987@Gmail.com>
2025-09-19 00:46:59 +0530

    Nav-Component-Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Nav from "./components/Nav.jsx";
 
 function App() {
 
   return (
     <>
-
+    <Nav />
     </>
   )
 }
